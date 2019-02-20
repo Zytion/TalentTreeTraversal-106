e982b7e991bc30b7b46567dcaefcbeba9c5f7ee7
Bobby Levin <bil1616@ad.rit.edu>
2019-02-20 09:20:15 -0500

    Added the 7 nodes to a tree

--- TalentTreeTraversal 106/Program.cs
+++ TalentTreeTraversal 106/Program.cs
@@ -10,6 +10,13 @@ namespace TalentTreeTraversal_106
     {
         static void Main(string[] args)
         {
+            TalentTreeNode node6 = new TalentTreeNode("Grappling Hook", true);
+            TalentTreeNode node5 = new TalentTreeNode("Double Dash", false);
+            TalentTreeNode node4 = new TalentTreeNode("Glide", false);
+            TalentTreeNode node3 = new TalentTreeNode("Wall Climb", false);
+            TalentTreeNode node2 = new TalentTreeNode("Double Jump", true, node4, node3);
+            TalentTreeNode node1 = new TalentTreeNode("Dash", true, node6, node5);
+            TalentTreeNode node0 = new TalentTreeNode("sprint", true, node1, node2);
         }
     }
 }
